a4071ce0cf47efd7a4aaa1275772ccb8f6950f66
OtavioWalk <otavio.carvalho@sptech.school>
2024-06-15 21:26:14 -0300

    Adição do banco de dados

---
+++ BD-Voll-Med.sql
@@ -0,0 +1,7 @@
+create database vollmed_api;
+
+-- em caso de erro na migration executar
+-- delete from flyway_schema_history where success = 0;
+-- drop database vollmed_api;
+
+drop table medicos;
